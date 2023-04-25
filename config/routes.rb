abefba82ee02664dec440b3f8f8e3ac9bf4d98f6
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-04-25 09:48:14 +0000

    コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:show, :edit, :index, :destroy, :create, :update] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
