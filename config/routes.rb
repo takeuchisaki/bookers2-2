511bdbf08b1b1a40ad6a649011871a34b6953946
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-04-25 07:25:33 +0000

    いいね機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :books, only: [:show, :edit, :index, :destroy, :create, :update]
+  resources :books, only: [:show, :edit, :index, :destroy, :create, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_24_100100) do
+ActiveRecord::Schema.define(version: 2023_04_25_063437) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2023_04_24_100100) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "body"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "homes", force: :cascade do |t|
