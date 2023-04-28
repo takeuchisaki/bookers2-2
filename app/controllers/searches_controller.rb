6be86843603147f662889e5b67067d7888fe95db
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-04-28 01:26:26 +0000

    [Finish] 応用課題5 検索機能訂正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,11 +4,12 @@ class SearchesController < ApplicationController
   def search
     @range =  params[:range]
     @word = params[:word]
+    @search = params[:search]
     if @range == "User"
-      @users = User.looks(params[:search], @word)
+      @users = User.looks(@search, @word)
       render "searches/search_result"
     else
-      @books = Book.looks(params[:search], @word)
+      @books = Book.looks(@search, @word)
       render "searches/search_result"
     end
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,18 +6,20 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :body, length: { in:1..200 }
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
-      @book = Book.where("title LIKE?", "#{word}")
-    elsif search == "partial_match"
-      @book = Book.where("title LIKE?", "%#{word}%")
+      Book.where("title LIKE?", "#{word}")
+    elsif search == "forward_match"
+      Book.where("title LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      Book.where("title LIKE?", "%#{word}")
     else
-      @book = Book.all
+      Book.where("title LIKE?", "%#{word}%")
     end
   end
 
