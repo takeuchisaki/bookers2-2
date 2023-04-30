d724863b0d931696f542453ed81d214ea173c453
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-04-30 13:21:29 +0000

    ゲストログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:show, :edit, :index, :destroy, :create, :update] do
@@ -12,5 +15,6 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   get 'search' => "searches#search"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
